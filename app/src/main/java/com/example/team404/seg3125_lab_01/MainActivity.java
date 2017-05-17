9e5559afe4f379d2eea451bb72b9c899ef3806ea
Daniel <dan.laframb@gmail.com>
2017-05-17 15:27:23 -0400

    First commit of tip calculator for android

---
+++ app/src/main/java/com/example/team404/seg3125_lab_01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.team404.seg3125_lab_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
